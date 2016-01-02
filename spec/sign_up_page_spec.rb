dc3c31f9f4c3a7e2b06f7f90b79f4510d15ab374
greenhairedlime <mariia.kalinichenko@nure.ua>
2016-01-02 18:13:56 +0200

    Added new features.

--- spec/sign_up_page_spec.rb
+++ spec/sign_up_page_spec.rb
@@ -24,4 +24,16 @@ RSpec.feature 'SignPage functionality' do
                         day: '22')
     expect(SignUpPage.given.text).to include('Please enter the same value again')
   end
+
+  scenario 'User can not pass the Getting Started section with incorrect credentials(Email and retyped email are different).' do
+    HomePage.open.open_sign_up_page
+    SignUpPage.given.fill_get_start(firstname: '',
+                                    lastname: '',
+                                    email: settings.def_test_user,
+                                    rtpemail: 'user@maiol.com',
+                                    month: '3',
+                                    year: '1989',
+                                    day: '22')
+    expect(SignUpPage.given.text).to include('Please enter the same value again')
+  end
 end
